a1252d8c2be672d7ce36758f072f93264ec75ca5
Gabriel Feo <gabriel@gabrielfeo.com>
2018-11-23 21:13:03 -0200

    Added a GUID to the Person model
    A GUID property was added to the Person class in order to make each
    Person unique despite the Name, and also implemented IEquatable<T>
    using the GUID.

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -2,15 +2,20 @@ using System;
 
 namespace Entities
 {
-    public struct Person
+    public struct Person : IEquatable<Person>
     {
+        public Guid id { get; }
         public string Name { get; set; }
         public Birthday Birthday { get; }
 
         public Person(string name, Birthday birthday)
         {
+            this.id = Guid.NewGuid();
             this.Name = name;
             this.Birthday = birthday;
         } 
+
+        public bool Equals(Person other) => this.HasId(other.id);
+        public bool HasId(Guid id) => this.id.Equals(id);
     }
 }
