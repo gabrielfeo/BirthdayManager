3c8cedb4a4f26ae41b6871cf446c0568bc92af39
Gabriel Feo <gabriel@gabrielfeo.com>
2018-12-03 18:50:39 -0200

    Created a ConsoleAdapter for list of people

--- ConsoleApp/ConsoleBirthdayManager.cs
+++ ConsoleApp/ConsoleBirthdayManager.cs
@@ -1,22 +1,27 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 using Entities;
 using Repository;
 using ConsoleApp.Adapter;
+using ConsoleApp.Resources;
 
 namespace ConsoleApp
 {
     internal class ConsoleBirthdayManager
     {
-
+        private static readonly TextWriter _consoleTextWriter = Console.Out;
         private readonly IRepository<Person> _personRepository = new RepositoryFactory().NewPersonRepository();
-        private readonly IConsoleAdapter<Person> _adapter = new PersonConsoleAdapter(Console.Out);
+        private readonly IConsoleAdapter<IEnumerable<Person>> _adapter = new PeopleConsoleAdapter(_consoleTextWriter);
 
         public void Start()
         {
-            foreach (var person in _personRepository.GetAll())
-            {
-                _adapter.Write(person);
-            }
+            var people = _personRepository.GetAll();
+            if (people.Count == 0) WriteEmptyRepositoryMessage();
+            else _adapter.Write(people);
         }
+
+        private void WriteEmptyRepositoryMessage() => _consoleTextWriter.WriteLine(Messages.ERROR_NO_PEOPLE_ADDED);
+
     }
 }
---
+++ ConsoleApp/Resources/Messages.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ConsoleApp.Resources
+{
+    public struct Messages
+    {
+        public const string APP_INTRO = "Welcome to the Birthday Manager!";
+        public const string ERROR_NO_PEOPLE_ADDED = "There's no one here yet. Please add somebody's birthday to continue.";
+    }
+}
